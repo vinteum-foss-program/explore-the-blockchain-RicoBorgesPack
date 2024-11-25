1f7cc70b6653d0f6685167f7826b70f0c2e6d3d8
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-11-25 15:26:44 -0300

    Update 003.sh with CLI Prompt

--- submission/003.sh
+++ submission/003.sh
@@ -1,10 +1,18 @@
 # How many new outputs were created by block 123,456?
+BLOCK_HASH=$(bitcoin-cli getblockhash 123456)
+TXIDS=$(bitcoin-cli getblock $BLOCK_HASH | jq -r '.tx[]')
 
+TOTAL_OUTPUTS=0
+for TXID in $TXIDS; do
+  OUTPUTS=$(bitcoin-cli getrawtransaction $TXID true | jq '.vout | length')
+  TOTAL_OUTPUTS=$((TOTAL_OUTPUTS + OUTPUTS))
+done
+
+echo $TOTAL_OUTPUTS
 #Python
-block_hash = call_rpc("getblockhash", [123456])
-block_data = call_rpc("getblock", [block_hash])
-print(f"Block Data: {block_data}")
+#block_hash = call_rpc("getblockhash", [123456])
+#block_data = call_rpc("getblock", [block_hash])
+#print(f"Block Data: {block_data}")
 
-# Número de transações no bloco
-num_txs = len(block_data["tx"])
-print(f"txs in block 123,456: {num_txs}")
+#num_txs = len(block_data["tx"])
+#print(f"txs in block 123,456: {num_txs}")
