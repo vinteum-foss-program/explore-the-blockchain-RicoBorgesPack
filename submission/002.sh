a730a5aea7e03b6c2e5a1576697c1402b916ab13
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-11-25 14:47:25 -0300

    Update 002.sh Spaces and UpperCase

--- submission/002.sh
+++ submission/002.sh
@@ -3,10 +3,10 @@
 #   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
 
-Address = "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
-Signature = "HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
-Message = "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
-bitcoin-cli verifymessage $Address $Signature $Message
+ADDRESS="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+SIGNATURE="HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
+MESSAGE="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+bitcoin-cli verifymessage $ADDRESS $SIGNATURE $MESSAGE
 
 #Python
 
