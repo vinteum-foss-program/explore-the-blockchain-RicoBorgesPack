a696c4dd91a867981c53f5039b0ee150af0d8e06
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-11-25 11:03:28 -0300

    Update 002.sh with Python changes

--- submission/002.sh
+++ submission/002.sh
@@ -2,3 +2,11 @@
 #   address: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
+
+#Python
+address = "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+signature = "HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
+message = "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+
+is_valid = call_rpc("verifymessage", [address, signature, message])
+print(f"Signature is Valid: {is_valid}")
