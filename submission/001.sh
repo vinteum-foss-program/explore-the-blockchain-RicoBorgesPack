0e82a3f5d7b2a2c4866c49e05c974f076cbf384c
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-11-25 14:35:38 -0300

    Update 001.sh with CLI command

--- submission/001.sh
+++ submission/001.sh
@@ -1,5 +1,7 @@
 # What is the hash of block 654,321?
 
+bitcoin-cli getblockhash 654321
+
 #Python
-block_hash = call_rpc("getblockhash", [654321])
-print(f"Hash: {block_hash}")
+#block_hash = call_rpc("getblockhash", [654321])
+#print(f"Hash: {block_hash}")
