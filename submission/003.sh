8f564ca07f3783b6763c088635f7f4c1c48d4c03
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-11-25 11:06:11 -0300

    Update 003.sh with Python

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,10 @@
 # How many new outputs were created by block 123,456?
+
+#Python
+block_hash = call_rpc("getblockhash", [123456])
+block_data = call_rpc("getblock", [block_hash])
+print(f"Block Data: {block_data}")
+
+# Número de transações no bloco
+num_txs = len(block_data["tx"])
+print(f"txs in block 123,456: {num_txs}")
