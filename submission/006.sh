12f050d93049af383a2651c2f9c3960c7afa2e08
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-12-03 09:28:17 -0300

    Update 006.sh with CLI Prompts

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,11 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+COINBASE_TXID=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 256128) | jq -r '.tx[0]')
+BLOCK_HASH=$(bitcoin-cli getblockhash 257343)
+TXIDS=$(bitcoin-cli getblock $BLOCK_HASH | jq -r '.tx[]')
+
+for TXID in $TXIDS; do
+  if bitcoin-cli getrawtransaction $TXID true | jq -e ".vin[] | select(.txid == \"$COINBASE_TXID\")" > /dev/null; then
+    echo $TXID
+    break
+  fi
+done
