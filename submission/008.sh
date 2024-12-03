376c86de323bfb1491c5431d2c8d5345ff4adcbc
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-12-03 11:02:17 -0300

    Update 008.sh CLI

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,9 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+def get_signing_pubkey(txid):
+    raw_tx = subprocess.run(["bitcoin-cli", "getrawtransaction", txid, "true"], capture_output=True, text=True).stdout
+    tx_data = json.loads(raw_tx)
+    return tx_data["vin"][0]["scriptSig"]["asm"].split()[1]
+
+txid = "e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+print(get_signing_pubkey(txid))
