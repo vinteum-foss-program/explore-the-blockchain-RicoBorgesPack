09baba44a1cdfff48a966fa1b43b930b5c79fff3
RicoBorgesPack <43553798+RicoBorgesPack@users.noreply.github.com>
2024-12-03 09:35:32 -0300

    Update 007.sh with CLI

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,10 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+BLOCK_HASH=$(bitcoin-cli getblockhash 123321)
+TXIDS=$(bitcoin-cli getblock $BLOCK_HASH | jq -r '.tx[]')
+
+for TXID in $TXIDS; do
+  UTXOS=$(bitcoin-cli gettxout $TXID 0 true)
+  if [ ! -z "$UTXOS" ]; then
+    echo $UTXOS | jq -r '.scriptPubKey.addresses[0]'
+  fi
+done
